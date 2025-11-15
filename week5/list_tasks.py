29920bc2b1239fa8e140ac80288ef4800a744c1b
AlexandruV97 <lucianalexandru194@gmail.com>
2025-11-15 12:21:17 +0000

    Added code to demonstrate a simple list*

---
+++ week5/list_tasks.py
@@ -0,0 +1,12 @@
+#Activity 1
+
+def directions():
+    steps = ["Move forward" , "Move backward" , "Move left" , "Move right"]
+    return steps
+
+
+def run_task1():
+    movements = directions()
+    print(movements)
+if __name__ == "__main__":
+    run_task1()

4f1f272f22ff420ee5d6c8d7bc893e7e7bebaf75
AlexandruV97 <lucianalexandru194@gmail.com>
2025-11-15 13:01:50 +0000

    Added the new activity related to index

--- week5/list_tasks.py
+++ week5/list_tasks.py
@@ -10,3 +10,21 @@ def run_task1():
     print(movements)
 if __name__ == "__main__":
     run_task1()
+
+#Activity 2
+
+def movements():
+    path = ["Move forward" , 10 ,"Move Backward" , 5, "move left" , 3, "move right", 1]
+    return path
+
+def run_task2():
+    print("Moving")
+    path = movements()
+
+    for i in range (0, len(path) , 2):
+        direction = path[i]
+        step = path[i+1]
+        print(f"{direction} for {step} steps")
+
+if __name__ == "__main__":
+    run_task2()
